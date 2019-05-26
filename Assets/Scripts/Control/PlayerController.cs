fad0b8c9c0ca500d61a249b8284bfacf00b4f3d1
Mike Walters <mike@beringbullet.com>
2019-05-26 08:55:49 -0500

    Base with movement

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Mover mover;
+
+    private void Start()
+    {
+        mover = GetComponent<Mover>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo;
+        bool hasHit = Physics.Raycast(ray, out hitinfo);
+
+        if (hasHit)
+        {
+            mover.MoveTo(hitinfo.point);
+        }
+    }
+
+}
