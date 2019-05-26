9662aa7856eed4a38b52bd60c689ee0a36b841a2
Mike Walters <mike@beringbullet.com>
2019-05-26 09:26:27 -0500

    Adding Target Raycast

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using RPG.Combat;
 using RPG.Movement;
 using UnityEngine;
 
@@ -5,7 +7,7 @@ namespace RPG.Contral
 {
     public class PlayerController : MonoBehaviour
     {
-
+        private Ray MouseRay => Camera.main.ScreenPointToRay(Input.mousePosition);
         Mover mover;
 
         private void Start()
@@ -16,6 +18,27 @@ namespace RPG.Contral
         // Update is called once per frame
         void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(MouseRay);
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
@@ -24,14 +47,14 @@ namespace RPG.Contral
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitinfo;
-            bool hasHit = Physics.Raycast(ray, out hitinfo);
+            bool hasHit = Physics.Raycast(MouseRay, out hitinfo);
 
             if (hasHit)
             {
                 mover.MoveTo(hitinfo.point);
             }
         }
+
     }
 }
