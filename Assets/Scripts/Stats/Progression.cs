58ac8400984efb66df46ae716880594a47056029
Mike Walters <mike@beringbullet.com>
2019-06-10 21:27:50 -0500

    starting Stats system

---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Pregression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] characterClass = null;
+
+        [Serializable]
+        class ProgressionCharacterClass
+        {
+            [SerializeField] CharacterClass characterClass;
+            [SerializeField] float[] health = null;
+            [SerializeField] float[] damange = null;
+        }
+    }
+}
